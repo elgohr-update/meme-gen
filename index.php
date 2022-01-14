b56daf1b3d69c38633621c1969f0bec2b2177265
Robin Jacobs <robin@robinj.be>
2022-01-14 13:09:47 +0100

    Make text consistent too when seed supplied

--- index.php
+++ index.php
@@ -8,16 +8,19 @@ require_once( 'functions.php' );
 $config = load_validate_config();
 
 $image = NULL;
+$text = NULL;
 if (!empty($_GET['seed'])) {
 	$seed = $_GET['seed'];
 	$imageIndex = crc32($seed) % count($config['memes']);
 	$imagesUrls = array_keys($config['memes']);
 	$image = $imagesUrls[$imageIndex];
+
+	$textIndex = crc32($seed) % count($config['memes'][$image]);
+	$text = $config['memes'][$image][$textIndex];
 } else {
 	$image = array_rand($config['memes']);
+	$text = $config['memes'][$image][array_rand($config['memes'][$image])];
 }
-
-$text = $config['memes'][$image][array_rand($config['memes'][$image])];
 
 $top_text    = $_GET['top'] ?: $text[0];
 $bottom_text = $_GET['bottom'] ?: $text[1];
